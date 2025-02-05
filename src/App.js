f10161577663520d63c55bf9edafdc1663050028
Vasyl Hladyshev <sipandbulocki@gmail.com>
2025-02-05 15:45:57 +0100

    changed

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import LogIn from './pages/logIn/LogIn';
 import Students from './pages/students/Students';
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   
